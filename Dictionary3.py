246ff9e2067080a3bd807e0dc531f129177b59a4
Krishna-BBSR17 <krishna.sahoo090@gmail.com>
2022-10-27 18:24:00 +0530

    MD.txt file added

---
+++ Dictionary3.py
@@ -0,0 +1,9 @@
+d = {'School':'Students', 'Cage':'Animal', 'Farm':'Cows', 'River':'Fish'}
+for i in d.keys():
+    print(i)
+for i in d.values():
+    print(i)
+for i in d.items():
+    print(i)
+for i in d:
+    print(d[i])
